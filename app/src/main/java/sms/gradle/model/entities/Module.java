76d9086ef40bbe74f1d2e0bddba656b903e641c2
Andrei Roughton <andrough@amazon.co.uk>
2025-03-28 14:00:43 +0000

    Add Module and ModuleDAO with unit tests

--- app/src/main/java/sms/gradle/model/entities/Module.java
+++ app/src/main/java/sms/gradle/model/entities/Module.java
@@ -1,3 +1,14 @@
 package sms.gradle.model.entities;
 
-public class Module {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Module {
+    private final int id;
+    private String name;
+    private String description;
+    private String lecturer;
+    private int courseId;
+}
