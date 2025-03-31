d60d08c038fa18a3bde1cd832f312bab685bf4cf
Luke Cheskin <lcheskin@imdb.com>
2025-03-31 11:28:42 +0100

    Add Admin and AdminDAO with unit tests

--- app/src/main/java/sms/gradle/model/entities/Admin.java
+++ app/src/main/java/sms/gradle/model/entities/Admin.java
@@ -1,3 +1,13 @@
 package sms.gradle.model.entities;
 
-public class Admin {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Admin {
+    private final int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
