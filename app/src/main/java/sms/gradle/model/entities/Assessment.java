075e3e9e8b43fe0e23e8ee6501977af9745f8019
Jaimewh <jaimewh@amazon.co.uk>
2025-04-03 21:58:48 +0100

    Added AssessmentDAO, Assessment, AssessmentDAOTest

--- app/src/main/java/sms/gradle/model/entities/Assessment.java
+++ app/src/main/java/sms/gradle/model/entities/Assessment.java
@@ -1,3 +1,15 @@
 package sms.gradle.model.entities;
 
-public class Assessment {}
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Assessment {
+    private int id;
+    private String name;
+    private String description;
+    private Date dueDate;
+    private int moduleId;
+}
