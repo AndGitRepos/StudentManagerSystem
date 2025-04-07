7845ffa99b3722eddb13a1b793d1674954130c29
Andrei Roughton <andrough@amazon.co.uk>
2025-04-07 17:04:37 +0100

    Add Session class

---
+++ app/src/test/java/sms/gradle/utils/Session/SessionTest.java
@@ -0,0 +1,50 @@
+package sms.gradle.utils.Session;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SessionTest {
+    @Test
+    void testIsValidSession() {
+        Session session = Session.getInstance();
+        assertFalse(session.getUser().isPresent());
+    }
+
+    @Test
+    void testSetUser() {
+        Session session = Session.getInstance();
+        User user = new User(1, UserType.ADMIN, "John", "Smith", "smith@sms.com");
+        session.setUser(user);
+        assertTrue(session.getUser().isPresent());
+
+        assertEquals(user, session.getUser().get());
+    }
+
+    @Test
+    void testClearSession() {
+        Session session = Session.getInstance();
+        User user = new User(1, UserType.ADMIN, "John", "Smith", "smith@sms.com");
+        session.setUser(user);
+        assertTrue(session.getUser().isPresent());
+        assertEquals(user, session.getUser().get());
+        session.clearSession();
+        assertFalse(session.getUser().isPresent());
+    }
+
+    @Test
+    void testGetInstance() {
+        Session session1 = Session.getInstance();
+        Session session2 = Session.getInstance();
+        assertEquals(session1, session2);
+    }
+
+    @Test
+    void testGetUser() {
+        Session session = Session.getInstance();
+        User user = new User(1, UserType.ADMIN, "John", "Smith", "smith@sms.com");
+        session.setUser(user);
+        assertTrue(session.getUser().isPresent());
+        assertEquals(user, session.getUser().get());
+    }
+}
